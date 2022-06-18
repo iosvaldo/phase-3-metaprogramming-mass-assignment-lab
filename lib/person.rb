1d5a63a7d8b40d3ae8be7ca6deba3a06ef3895a6
osvaldo <iosvaldo.app@gmail.com>
2022-06-18 13:38:36 -0400

    commit tree 0ed61a1b8122b10b80aacb069d87917807273d82

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
   # your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+:weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(attributes = nil)
+    attributes.map do |key,value|
+      self.send("#{key}=",value)
+    end
+  end
+
+def greeting(name, programming_language)
+  puts "Hello, #{name}"
+  puts "Welcome to the #{programming_language} curriculum"
+end
+
+greeting(programming_language: "Ruby", name: "Leroy Jenkins")
+
 end
